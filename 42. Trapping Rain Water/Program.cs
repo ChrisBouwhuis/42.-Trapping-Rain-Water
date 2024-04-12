aeabfa653bb12fbaa44f98397df8bfce498a1a29
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-12 10:07:08 +0200

    Add project files.

---
+++ 42. Trapping Rain Water/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLib;
+namespace _42._Trapping_Rain_Water
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] height = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int result = solution.Trap(height);
+            Console.WriteLine(result);
+        }
+    }
+}
